5b1a026b8c0ba55d5f026b1cc1a4d00edeaa5f2c
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-24 07:36:18 +0100

    Update character scene

--- assets/scenes/character/scene.js
+++ assets/scenes/character/scene.js
@@ -23,14 +23,26 @@
 
         var $speechContainer = $('.speech-bubble-container', scene.$element);
         $speechContainer.removeClass('hidden');
-        // This looks weird and it is
+
         $speechContainer.css('left', $target.position().left + ($target.width() / 4) + 'px');
         $speechContainer.css('top', ($target.position().top - 100) + 'px');
       });
     })(i);
   }
 
+  scene.bindEvent('[data-behavior~=unselect]', function() {
+    $('.background', scene.$element).attr('data-active', 'none');
+    selectedCharacter = '';
+    $('.continue', scene.$element).addClass('disabled');
+    $('.speech-bubble-container', scene.$element).addClass('hidden');
+  });
+
   scene.bindEvent('[data-behavior~=continue]', function() {
+    // TODO: This should be data-disabled or similar
+    if ($(this).hasClass('disabled')) {
+      return;
+    }
+
     world.character = selectedCharacter;
     scene.parent.show('game');
   });
