f0e2d87a29cce78fe406470c08901a1abe81ad40
Dawid Pach <polooo2@hotmail.com>
2015-01-24 18:27:15 +0100

    adds intro and credits, updates credits in case of unsuccessful

--- assets/scenes/outro/scene.js
+++ assets/scenes/outro/scene.js
@@ -1,7 +1,13 @@
 (function(scene) {
+  var world = scene.parent.parent.world;
   
   scene.bindEvent('[data-behavior~=back]', function() {
-    scene.parent.show('character');
+    scene.parent.show('intro');
   });
-
+  scene.on('active', function() {
+    // check if the player has enough hearths for the win situation
+    if (world.character.hearts === 3) {
+      $('.background', event.$element).addClass('success');
+    }
+  });
 })(this);
